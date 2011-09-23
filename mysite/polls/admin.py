3e217b656236f292556caf6e017c9d147d53afd0
Rob Fisher <rfisher@nds.com>
2011-09-23 18:42:12 +0100

    Enable filtering, searching and hierarchy in the admin interface

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -12,5 +12,8 @@ class PollAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
     list_display = ('question', 'pub_date', 'was_published_today')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
 
 admin.site.register(Poll, PollAdmin)
