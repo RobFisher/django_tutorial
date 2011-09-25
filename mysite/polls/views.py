f1355ed4eed512203509a543126ed343598a1d21
Rob Fisher <rfisher@nds.com>
2011-09-25 09:57:56 +0100

    Poll detail view raises 404 when requested poll does not exist

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,7 +7,11 @@ def index(request):
     return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    try:
+        p = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render_to_response('polls/detail.html', {'poll': p})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
